08840731297c7f2d14f29e13d6955616ac5af225
Raave Aires <developer@raave.me>
2026-02-07 11:27:41 -0300

    feat: Adicione `allowedDevOrigins` às configs do Next.js.

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  allowedDevOrigins: ["dev.rl2.me"],
 };
 
 export default nextConfig;
